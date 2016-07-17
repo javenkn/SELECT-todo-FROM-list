6873d17bae3687953f625366759e1f9806823788
javenkn <javenkn7@yahoo.com>
2016-07-16 17:29:02 -1000

    finished up to the adding a column named completed_at to the table.

---
+++ todo_app.sql
@@ -0,0 +1,29 @@
+-- Write a query to drop a database named todo_app if it exists
+DROP DATABASE IF EXISTS todo_app;
+-- Write a query to create a database named todo_app
+CREATE DATABASE todo_app;
+-- Write a query to create a user named michael with an encrypted password stonebreaker
+DROP USER IF EXISTS michael;
+-- Write a query to delete a user named michael if it exists
+CREATE USER michael
+  WITH
+    ENCRYPTED PASSWORD 'stonebreaker';
+
+\c todo_app;
+
+DROP TABLE IF EXISTS tasks;
+
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) UNIQUE NOT NULL,
+  description text NULL,
+  created_at timestamp NOT NULL DEFAULT now(),
+  updated_at timestamp NULL,
+  completed boolean NOT NULL DEFAULT false
+);
+
+ALTER TABLE IF EXISTS tasks
+  DROP COLUMN IF EXISTS completed;
+
+ALTER TABLE IF EXISTS tasks
+  ADD COLUMN completed_at timestamp NULL DEFAULT NULL;
